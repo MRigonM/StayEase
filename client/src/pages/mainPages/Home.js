2bbc9177f60959d6e3cdccd9b6daf031b95b28d9
Edoni78 <edonaliu8@gmail.com>
2025-03-31 21:47:48 +0200

    client setup/installed packages

---
+++ client/src/pages/mainPages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+/* bahen import komponentat */
+/*Kjo komponent (Home) bahet import ne app.js*/  
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
