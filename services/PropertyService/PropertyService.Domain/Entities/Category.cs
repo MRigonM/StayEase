f06cfc9360652de6f3943dd554f3016fcc43ba9f
Rigon <rigonmej@gmail.com>
2025-04-10 20:00:55 +0200

    BNB-71-Added Country and Location Entities

--- services/PropertyService/PropertyService.Domain/Entities/Category.cs
+++ services/PropertyService/PropertyService.Domain/Entities/Category.cs
@@ -3,4 +3,5 @@
 public class Category: BaseEntity<int>
 {
     public string Name { get; set; } = string.Empty;
+    public virtual ICollection<PropertyCategory> PropertyCategories { get; set; } = new HashSet<PropertyCategory>();
 }
---
+++ services/PropertyService/PropertyService.Domain/Entities/Country.cs
@@ -0,0 +1,8 @@
+﻿
+namespace PropertyService.Domain.Entities;
+
+public class Country: BaseEntity<int>
+{
+    public string Name { get; set; } = string.Empty;
+    public virtual ICollection<Location> Locations { get; set; } = new HashSet<Location>(); 
+}
