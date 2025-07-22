e1328730aa60e8bf7e0a0b05ccdc1770772f7482
Rigon <rigonmej@gmail.com>
2025-07-22 19:03:52 +0200

    Added needed DTOs, Interfaces and Entities for Identity

---
+++ StayEase.Domain/DataTransferObjects/RegisterDTO.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json.Serialization;
+
+namespace StayEase.Domain.DataTransferObjects
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Role
+    {
+        Customer,
+        Owner 
+    }
+    
+    public class RegisterDTO
+    {
+        public string FirstName { get; set; }
+        public string MiddlName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public IFormFile? Image { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public IEnumerable<Role> roles { get; set; } = new List<Role>();
+
+    }
+}
