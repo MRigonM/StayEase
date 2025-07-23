d0bc05fb100866e7cccc49645fbc29356dc60ded
Rigon <rigonmej@gmail.com>
2025-07-23 23:45:08 +0200

    Added Extensions and Validators updated appsettings and launchsettings

---
+++ StayEase.APIs/Extensions/ApplicationServices.cs
@@ -0,0 +1,58 @@
+﻿using StayEase.Application.Resolvers;
+using StayEase.Application.Services;
+using StayEase.Application.Settings;
+using StayEase.Domain.Identity;
+using StayEase.Domain.Interfaces.Services;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+using StayEase.APIs.Validators;
+
+namespace StayEase.APIs.Extensions;
+
+public static class ApplicationServices
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection Services, IConfiguration Configuration)
+        {
+            Services.AddDbContext<DbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                options.UseLazyLoadingProxies();
+            });
+
+
+            Services.AddSingleton<IConnectionMultiplexer>(Options =>
+            {
+                var Connection = Configuration.GetConnectionString("DefaultConnection");
+                return ConnectionMultiplexer.Connect(Connection);
+            });
+            // Identity Configurations
+            Services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedEmail = true;
+                options.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultEmailProvider;
+            })
+            .AddEntityFrameworkStores<DbContext>()
+            .AddDefaultTokenProviders();
+
+            // AutoMapper Configuration
+            Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); 
+            Services.AddMemoryCache();
+            Services.AddScoped<UserResolver>();
+            Services.AddScoped<IAuthService, AuthService>();
+            Services.AddScoped<IUserService, UserService>();
+            Services.AddHttpContextAccessor();
+            
+
+            Services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
+            Services.AddTransient<IMailService, MailService>();
+
+            Services.AddFluentValidation(fv =>
+            {
+                fv.RegisterValidatorsFromAssembly(typeof(CreateAccountValidator).Assembly);
+            });
+
+            return Services;
+        }
+    }
---
+++ StayEase.APIs/Validators/ForgetPasswordValidation.cs
@@ -0,0 +1,13 @@
+﻿using StayEase.Domain.DataTransferObjects;
+using FluentValidation;
+
+namespace StayEase.APIs.Validators
+{
+    public class ForgetPasswordValidation:AbstractValidator<ForgetPasswordDto>
+    {
+        public ForgetPasswordValidation()
+        {
+            RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        }
+    }
+}
