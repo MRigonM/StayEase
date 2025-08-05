a5642358a8f31db6643318b3a30591ce190bd862
Rigon <rigonmej@gmail.com>
2025-08-06 00:33:30 +0200

    Added IPropertyService and PropertyService

---
+++ StayEase.Application/Services/PropertyService.cs
@@ -0,0 +1,33 @@
+﻿using StayEase.Domain;
+using StayEase.Domain.DataTransferObjects.Property;
+using StayEase.Domain.Interfaces.Services;
+
+namespace StayEase.Application.Services;
+
+public class PropertyService : IPropertyService
+{
+    public async Task<Responses> GetAllPropertiesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Responses> GetPropertyByIdAsync(string propertyId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Responses> CreatePropertyAsync(string? email, PropertyToCreateDTO propertyDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Responses> UpdatePropertyAsync(string propertyId, PropertyToUpdateDTO propertyDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Responses> DeletePropertyAsync(string propertyId)
+    {
+        throw new NotImplementedException();
+    }
+}
