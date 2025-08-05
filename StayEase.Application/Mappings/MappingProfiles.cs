b016bebb44e59e063d0f1da9479d8218505c8dc6
Rigon <rigonmej@gmail.com>
2025-08-05 14:11:32 +0200

    Updates for fixing AccountService

--- StayEase.Application/Mappings/MappingProfiles.cs
+++ StayEase.Application/Mappings/MappingProfiles.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using StayEase.Application.Resolvers;
 using StayEase.Domain.DataTransferObjects;
 using StayEase.Domain.Identity;
 
@@ -8,6 +9,15 @@ public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
+        CreateMap<AppUser, UserDTO>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName))
+            .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.UserName))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
+            .ForMember(dest => dest.profileImage, opt => opt.MapFrom<UserResolver>());
+        
         CreateMap<AppUser, OwnerDto>()
             .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FullName))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
