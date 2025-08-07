32c9b040417f2dfe8ecbf52c4f58c1ed887fc713
Rigon <rigonmej@gmail.com>
2025-08-07 21:59:11 +0200

    Added GenericRepo, UnitOfWork and Specifactions

---
+++ StayEase.Infrastructure/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StayEase.Domain;
+using StayEase.Domain.Interfaces.Repositories;
+
+namespace StayEase.Infrastructure.Specifications
+{
+    public static class SpecificationEvaluator<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> InputQuery, ISpecifications<TEntity, TKey> Spec)
+        {
+            var Query = InputQuery;
+            if (Spec.Criteria is not null)
+            {
+                Query = Query.Where(Spec.Criteria);
+            }
+
+            if (Spec.OrderBy is not null)
+            {
+                Query = Query.OrderBy(Spec.OrderBy);
+            }
+
+            if(Spec.OrderByDescending is not null)
+            {
+                Query = Query.OrderByDescending(Spec.OrderByDescending);
+            }
+
+            if(Spec.IsPaginationEnabled)
+            {
+                Query = Query.Skip(Spec.Skip).Take(Spec.Take);
+            }
+
+            Query = Spec.Includes.Aggregate(Query, (CurrentQuery, IncludeExpression) => CurrentQuery.Include(IncludeExpression));
+            return Query;
+        }
+    }
+}
