c35a5a7cb2fece3b8c43d4e627d4d2f6811394d8
Rigon <rigonmej@gmail.com>
2025-08-12 20:19:07 +0200

    Adding Payment, added IPayment and StripeSettings

---
+++ StayEase.Domain/Interfaces/Services/IPaymentService.cs
@@ -0,0 +1,11 @@
+﻿namespace StayEase.Domain.Interfaces.Services
+{
+    public interface IPaymentService
+    {
+        Task<Responses> CreatePaymentIntentAsync(string currency, int bookingId);
+        Task<Responses> CreateCheckoutSessioinAsync(int bookingId);
+        Task<Responses> RefundPaymentAsync(int bookingId);
+        Task<Responses> PaymentSuccessAsync(int bookingId);
+        Task<Responses> PaymentCancelAsync(int bookingId);
+    }
+}
