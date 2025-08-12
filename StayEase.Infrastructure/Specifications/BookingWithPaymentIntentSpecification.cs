e9f6e85d9c9eb97c8b09496b6f9ae4326ee965b2
Rigon <rigonmej@gmail.com>
2025-08-12 20:01:01 +0200

    Added Booking Specifications

---
+++ StayEase.Infrastructure/Specifications/BookingWithPaymentIntentSpecification.cs
@@ -0,0 +1,12 @@
+﻿using StayEase.Domain.Entities;
+
+namespace StayEase.Infrastructure.Specifications
+{
+    public class BookingWithPaymentIntentSpecification : BaseSpecifications<Booking, int>
+    {
+        public BookingWithPaymentIntentSpecification(string PaymentIntentId) : base(B => B.PaymentIntentId == PaymentIntentId)
+        {
+
+        }
+    }
+}
