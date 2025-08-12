2d4c8973c9ecf30e4bdcb43faf3ce137f1b5dd9f
Rigon <rigonmej@gmail.com>
2025-08-12 19:51:50 +0200

    Updates at DTOs and Added IBookService

---
+++ StayEase.Domain/Interfaces/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using StayEase.Domain.DataTransferObjects.Booking;
+
+namespace StayEase.Domain.Interfaces.Services
+{
+    public interface IBookService
+    {
+        Task<Responses> GetBookingsByUserId(string email);
+        Task<Responses> GetBookingById(int bookingId);
+        Task<Responses> CreateBookingByPropertyId(string email, BookingToCreateDTO bookDTO);
+        Task<Responses> UpdateBookingByPropertyId(int bookingId, BookingToUpdateDTO bookDTO);
+        Task<Responses> DeleteBookingById(int bookingId);
+    }
+}
